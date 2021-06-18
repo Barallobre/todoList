eba6e9c6d85190a678b57ce2614347f2bcb6939e
Barallobre <cristianbarallobrerua@gmail.com>
2021-06-18 22:38:17 +0200

    Las tareas permanecen en el local storage

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState("all");
   const [filteredTodos, setFilteredTodos] = useState([]);
-
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
   useEffect(() => {
     filterHandler();
+    saveLocalTodos();
   }, [todos, status]);
 
   const filterHandler = () => {
@@ -27,6 +30,18 @@ function App() {
     }
   };
 
+  const saveLocalTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem("todos"));
+      console.log(todoLocal);
+      setTodos(todoLocal);
+    }
+  };
   return (
     <div className="App">
       <header>
