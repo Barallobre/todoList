f2463d514013c415a0cd743bd39fae1a99b2c932
Barallobre <cristianbarallobrerua@gmail.com>
2021-06-18 21:56:32 +0200

    cambios en los estilos

--- src/App.js
+++ src/App.js
@@ -1,43 +1,51 @@
-import {useState, useEffect} from 'react';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-import Form from './componentes/Form';
-import TodoList from './componentes/todoList';
+import Form from "./componentes/Form";
+import TodoList from "./componentes/todoList";
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
 
-  
-  
-  const[inputText,setInputText] = useState("");
-  const[todos,setTodos] = useState([]);
-  const[status,setStatus] = useState('all');
-  const[filteredTodos,setFilteredTodos] = useState([]);
-  
-  useEffect(()=>{
+  useEffect(() => {
     filterHandler();
-  },[todos,status]);
+  }, [todos, status]);
 
-  const filterHandler = () =>{
-    switch(status){
-      case 'completed':
-        setFilteredTodos(todos.filter(todo => todo.completed === true));
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
         break;
-      case 'uncompleted':
-        setFilteredTodos(todos.filter(todo => todo.completed === false));
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
         break;
       default:
         setFilteredTodos(todos);
         break;
     }
-}
-
+  };
 
   return (
     <div className="App">
       <header>
-        <h1>Lista de Tareas de Cristian</h1>
+        <h1>Lista de Tareas</h1>
       </header>
-      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} setStatus={setStatus} status={status}/>
-      <TodoList setTodos={setTodos} todos={todos} filteredTodos={filteredTodos} status={status}/>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        setStatus={setStatus}
+        status={status}
+      />
+      <TodoList
+        setTodos={setTodos}
+        todos={todos}
+        filteredTodos={filteredTodos}
+        status={status}
+      />
     </div>
   );
 }
